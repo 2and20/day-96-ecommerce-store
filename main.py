c8ae53a742224623b904910569a907a7a059d740
adutch <alandutch@gmail.com>
2022-08-12 13:19:49 +0100

    registering now auto-logs in.

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
 from flask import Flask, render_template, redirect,\
     url_for, request, flash
-# from flask import Blueprint
 from flask_login import LoginManager, UserMixin, login_user, \
     logout_user, current_user, login_required
-# from flask_login import login_required,
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm, RegisterForm, ItemForm
 from werkzeug.security import generate_password_hash, check_password_hash
 import stripe
 import os
-import smtplib
 from flask_mail import Mail, Message
-import pprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
 # app.config.from_pyfile('config.py')
 
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
-
 Bootstrap(app)
 user_list = []
 
-# login_manager = LoginManager()
 login_manager = LoginManager(app)
-# login_manager.init_app(app)
-# from app import login
 
 # this part is just to redirect after stripe to /success or /cancel
-# YOUR_DOMAIN = "https://rightclicksave.herokuapp.com"
-# YOUR_DOMAIN = "http://127.0.0.1:5000"
+# different for local versus heroku hosted
 YOUR_DOMAIN = os.environ["YOUR_DOMAIN"]
 
 #couldnt get this if-else to work in heroku without crashing
@@ -39,20 +28,15 @@ YOUR_DOMAIN = os.environ["YOUR_DOMAIN"]
 #     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 # else:
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["SQLALCHEMY_DATABASE_URI"]
-    # 'sqlite:///shop-data.db'
 db = SQLAlchemy(app)
-# stripe publishable key = 'pk_test_51LRzYfLZ6eSqh5K0hTxEB5dkUEnethTKOYWOOTzOraYeRLTm8W6iKElQb8OdLVUwgzJ08B7KEu5bdsOsItrk8TIC00fjH3PjX6'
-# stripe.api_key = "sk_test_51LRzYfLZ6eSqh5K0DeRnCpsV3A0wSYIkdXR6REAcCXTiCLLymdXZRt9uECm20ua8bO808AoDuCMzp3r0lQHXO3cZ00rbqOPCFV"
 BASE_URL = 'https://api.stripe.com'
 
-# stripe_keys = {"stripe_secret_key":os.environ["STRIPE_SECRET_KEY"],
-#                "stripe_pub_key":os.environ["STRIPE_PUB_KEY"]}
+stripe_keys = {"stripe_secret_key":os.environ["STRIPE_SECRET_KEY"],
+               "stripe_pub_key":os.environ["STRIPE_PUB_KEY"]}
 #### live keys above, test keys below
-stripe_keys = {"stripe_secret_key":os.environ["STRIPE_TEST_SECRET_KEY"],
-               "stripe_pub_key":os.environ["STRIPE_TEST_PUB_KEY"]}
+# stripe_keys = {"stripe_secret_key":os.environ["STRIPE_TEST_SECRET_KEY"],
+#                "stripe_pub_key":os.environ["STRIPE_TEST_PUB_KEY"]}
 stripe.api_key = stripe_keys["stripe_secret_key"]
-
-
 
 # TEST CARDS TO USE ON STRIPE
 # Payment succeeds
@@ -61,7 +45,6 @@ stripe.api_key = stripe_keys["stripe_secret_key"]
 # 4000 0025 0000 3155
 # Payment is declined
 # 4000 0000 0000 9995
-
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
@@ -84,11 +67,6 @@ class User(UserMixin, db.Model):
     address2 = db.Column(db.String)
     city = db.Column(db.String)
     postcode = db.Column(db.String)
-
-# class User2(db.Model):
-#     __tablename__ = 'users2'
-#     id = db.Column(db.Integer, nullable=True, primary_key=True)
-#     food = db.Column(db.String)
 
 class Cart(db.Model):
     __tablename__ = 'cart'
@@ -126,7 +104,6 @@ db.session.commit()
 def load_user(user_id):
     if user_id is None:
         return None
-    # return User.query.get(id)
     return User.query.get(user_id)
 
 def total_price():
@@ -135,18 +112,14 @@ def total_price():
     total_price = 0
     for thing in all_items:
         for to_buy in cart_stuff:
-            print(f"to buy.id {to_buy.item}, thing.item_id {thing.item_id}")
             if str(to_buy.item) == str(thing.item_id):
-                print(f"total price is {total_price}")
                 total_price += thing.item_px * to_buy.num_items
     return total_price
 
 @app.route('/')
 def homepage():
-    print(current_user)
     all_items = Items.query.all()
     return render_template("index3.html", user=current_user, all_items=all_items)
-
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -155,18 +128,14 @@ def login():
         username = form.username.data
         password = form.password.data
         user = User.query.filter_by(username=username).first()
-        # print(f"user is {user}, user.id is type: {type(user.id)}")
         if user == None:
             pass
             flash("Email not found, please Register")
             render_template("login2.html", form=form)
         else:
             if check_password_hash(user.password, password):
-                print("password matches hash")
                 login_user(user)
-                # return render_template("index.html")
                 return redirect(url_for('homepage'))
-                # how do i make the user be logged in?
             else:
                 flash("Incorrect password")
                 render_template("login2.html", form=form)
@@ -192,16 +161,15 @@ def register():
                         address2=address2,
                         city=city,
                         postcode=postcode)
-        print(f"jj {User.query.filter_by(username=username).first()}")
         if User.query.filter_by(username=username).first() is None:
             db.session.add(new_user)
             db.session.commit()
+            # below logs newly registered user straight in
             user = User.query.filter_by(username=username).first()
             login_user(user)
             return redirect(url_for('homepage'))
         else:
             flash("That email is already registered, login instead?")
-
     return render_template("register2.html", form=form)
 
 @app.route('/cart', methods=["GET","POST"])
@@ -210,27 +178,20 @@ def order_cart():
         # can only view/add to cart if logged in
         return redirect(url_for("login"))
     if request.method=="POST":
-        # rope kettle dumb
-        print(f"a {(request.form.to_dict().values())}")
-
         if list(request.form.to_dict().values())[0] == "Delete Item":
             item_delete = list(request.form.to_dict().keys())[0]
             to_delete = Cart.query.filter_by(item=item_delete, user_id=current_user.id).first()
             db.session.delete(to_delete)
             db.session.commit()
             return redirect(url_for('order_cart'))
-        # except IndexError:
-        #     pass
         print(request.form)
         item_add = list(request.form.to_dict().keys())[0]
-        # form_stuff returns the item that was clicked (eg rope)
+        # form_stuff returns the item that was clicked
         print(item_add)
 
         # now need to check how many items already in cart for this user,
         # and add 1 to whichever was clicked
-        print(f"current_user.id type is {type(current_user.id)}")
         prev_order = Cart.query.filter_by(item=item_add, user_id=current_user.id).first()
-        print(f"prev_order {prev_order}")
         if prev_order is None:
             new_order=Cart(item=item_add, num_items=1, user_id=current_user.id)
             db.session.add(new_order)
@@ -239,11 +200,6 @@ def order_cart():
             prev_order.num_items = prev_order.num_items +1
             #prev_order is actually a line object
             db.session.commit()
-        # id = db.Column(db.Integer, nullable=True, primary_key=True)
-        # item = db.Column(db.String)
-        # num_items = db.Column(db.String)
-        # user = db.Column(db.Integer)
-        # return "OK that's added!"
     purchases= Cart.query.filter_by(user_id=current_user.id).all()
     all_items = Items.query.all()
     checkout_price = total_price()
@@ -254,7 +210,6 @@ def order_cart():
 def logging_out():
     logout_user()
     return render_template("logout.html")
-    # return redirect(url_for('homepage'))
 
 @app.route('/create-checkout-session', methods=['POST'])
 def create_checkout_session():
@@ -281,9 +236,6 @@ def create_checkout_session():
             success_url= YOUR_DOMAIN + '/success?session_id={CHECKOUT_SESSION_ID}',
             cancel_url= YOUR_DOMAIN + '/cancel?session_id={CHECKOUT_SESSION_ID}',
         )
-
-        # http://127.0.0.1:5000
-        # 'price': '{{PRICE_ID}}',
     except Exception as e:
         return str(e)
 
@@ -298,10 +250,7 @@ def success():
             return redirect(url_for("order_cart"))
     except:
         return redirect(url_for("order_cart"))
-    print(f"session: {session}")
-    print(f"\n\n\ncustomer: {customer}")
-    print(f"\ncustomer.name: {customer.name}")
-    print(f"\n\nsession.status: {session.status}")
+
     purchases = Cart.query.filter_by(user_id=current_user.id).all()
     all_items = Items.query.all()
     buyer = User.query.filter_by(id=current_user.id).first()
@@ -312,11 +261,9 @@ def success():
     email_body = ""
     for purchase in purchases:
         # this lets me put the number of items into success.html,
-        # whilst deleting from the cart since they've been
-        # purchased
+        # whilst deleting from the cart since they've been purchased
         this_item = Items.query.filter_by(item_id=int(purchase.item)).first()
         image_link_full = this_item.item_image.split("?")[0]
-        print(f"image link is {image_link_full}")
         email_body += f"<img src='{this_item.item_image}'><br><a href='{image_link_full}'>Download Link</a><br><br>"
         final_order=purchase
         final_orders.append(final_order)
@@ -356,8 +303,6 @@ def cancel():
 @app.route('/admin', methods=['GET','POST'])
 def admin():
     form = ItemForm()
-    print(f"request.method is {request.method}")
-
     try: # checks that user is id=1, ie. admin
         if current_user.id != 1:
             return redirect(url_for('homepage'))
@@ -365,7 +310,6 @@ def admin():
         return redirect(url_for('homepage'))
 
     if form.validate_on_submit():
-        print("got here")
         new_item = Items(item_name=form.item_name.data,
                         item_image=form.item_image.data,
                         item_desc=form.item_desc.data,
@@ -377,20 +321,14 @@ def admin():
     if request.method=="POST":
         try:
             item_to_delete = int(list(request.form.to_dict().keys())[0])
-            print(f"item to delete {item_to_delete}, type {type(item_to_delete)}")
             del_this_object = Items.query.filter_by(item_id = item_to_delete).first()
-            print(del_this_object)
             db.session.delete(del_this_object)
             db.session.commit()
         except:
             pass
         return redirect(url_for('admin'))
-        # print(f"a {(request.form.to_dict().keys())}")
-        # if list(request.form.to_dict().values())[0] == "additem":
 
     all_items = Items.query.all()
-    print(f"all_items: {all_items}")
-
     return render_template('admin.html', all_items=all_items, form=form)
 
 @app.route('/about')
